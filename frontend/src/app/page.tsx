74e13e6d2712e63667931ce1b0d776ccd69a003e
ramosbry1398-sys <ramos.bry1398@gmail.com>
2026-07-23 12:24:51 -0500

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '../utils/supabase/server'
 
 export default async function Home() {
   const supabase = await createClient()
